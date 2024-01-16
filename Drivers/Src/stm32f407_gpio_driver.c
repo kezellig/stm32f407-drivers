8b66e2d6d0173fe6eacb483fa766e95f333bb358
Kaitlyn <kaitlyn@kaitlyn.works>
2024-01-15 22:01:59 -0500

    Create first version of GPIO init

--- Drivers/Src/stm32f407_gpio_driver.c
+++ Drivers/Src/stm32f407_gpio_driver.c
@@ -13,6 +13,7 @@
  * @param: Whether the clock is enabled or disabled - ENABLE or DISABLE macros
  */
 void GPIO_PClockControl(GPIO_RegDef_t *p_GPIOx, uint8_t EnDi) {
+	// Replace this stuff with a function pointer array in the future
 	if (EnDi == ENABLED) {
 		if (p_GPIOx == GPIOA) {
 			GPIOA_PCLK_EN();
@@ -61,8 +62,40 @@ void GPIO_PClockControl(GPIO_RegDef_t *p_GPIOx, uint8_t EnDi) {
  * Initialises a GPIO port
  * @param: A GPIO Handle containing GPIO configs and GPIO port base address
  */
-void GPIO_Init(GPIO_Handle_t *p_GPIO_Handle) {
+void GPIO_Init(GPIO_Handle_t *p_GPIOHandle) {
+	uint32_t temp = 0;
+	if (p_GPIOHandle->GPIO_PinConfig.GPIO_PinMode <= GPIO_MODE_ANALOG) {
+		// Modes from [0, 3] are non-interrupting
+		// Retrieve mode: each pin takes two bit fields, hence multiply pin no. by 2
+		temp = (p_GPIOHandle->GPIO_PinConfig.GPIO_PinMode << (2 *p_GPIOHandle->GPIO_PinConfig.GPIO_PinNumber));
 
+		// Store our mode in MODER - port mode register
+		p_GPIOHandle->p_GPIOx->MODER = temp;
+
+	} else {
+
+	}
+	temp = 0;
+
+	// Speed config - 2 bit fields
+	temp = (p_GPIOHandle->GPIO_PinConfig.GPIO_PinSpeed << (2 *p_GPIOHandle->GPIO_PinConfig.GPIO_PinNumber));
+	p_GPIOHandle->p_GPIOx->OSPEEDR = temp;
+	temp = 0;
+
+	// PUPD config -  2 bit fields
+	temp = (p_GPIOHandle->GPIO_PinConfig.GPIO_PinPuPdControl << (2 *p_GPIOHandle->GPIO_PinConfig.GPIO_PinNumber));
+	p_GPIOHandle->p_GPIOx->PUPDR = temp;
+	temp = 0;
+
+	// Output type config - 1 bit field
+	temp = (p_GPIOHandle->GPIO_PinConfig.GPIO_PinOPType << p_GPIOHandle->GPIO_PinConfig.GPIO_PinNumber);
+	p_GPIOHandle->p_GPIOx->OTYPER = temp;
+	temp = 0;
+
+	// Alternate function config
+	if (p_GPIOHandle->GPIO_PinConfig.GPIO_PinMode == GPIO_MODE_ALTFN) {
+		// Do something with alt. fun. registers
+	}
 }
 
 /**
@@ -82,7 +115,8 @@ void GPIO_DeInit(GPIO_RegDef_t *p_GPIOx) {
 uint8_t GPIO_ReadFromInputPin(GPIO_RegDef_t *p_GPIOx, uint8_t PinNumber) {
 
 }
-uint16_t GPIO_ReadFromInputPort(GPIO_RegDef_t *p_GPIOx){
+
+uint16_t GPIO_ReadFromInputPort(GPIO_RegDef_t *p_GPIOx) {
 
 }
 
