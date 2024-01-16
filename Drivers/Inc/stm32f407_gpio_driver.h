bdb9ec00782fedf789905cd0aa5a986b9987088a
Kaitlyn <kaitlyn@kaitlyn.works>
2024-01-15 21:03:37 -0500

    Add more GPIO config macros

--- Drivers/Inc/stm32f407_gpio_driver.h
+++ Drivers/Inc/stm32f407_gpio_driver.h
@@ -11,13 +11,14 @@
 // Configuration properties of a GPIO pin in a struct
 typedef struct
 {
-	uint8_t GPIO_PinNumber; // 0-15 available
-	uint8_t GPIO_PinMode;
-	uint8_t GPIO_PinSpeed;
-	uint8_t GPIO_PinPuPdControl; // Pullup/Pulldown
-	uint8_t GPIO_PinOPType; // Output type
-	uint8_t GPIO_PinAltFunMode; // Alternative function mode
+	uint8_t GPIO_PinNumber; 		// Values from @GPIO_PIN_NUMBERS
+	uint8_t GPIO_PinMode;			// Values from @GPIO_PIN_MODES
+	uint8_t GPIO_PinPuPdControl; 	// Values from @GPIO_PIN_PUPD
+	uint8_t GPIO_PinOPType; 		// Values from @GPIO_OP_TYPE
+	uint8_t GPIO_PinSpeed;			// Values from @GPIO_OP_SPEEDS
+	uint8_t GPIO_PinAltFunMode; 	// Alternative function mode
 } GPIO_PinConfig_t;
+
 
 // GPIO pin handle - address and config settings
 typedef struct
@@ -25,6 +26,68 @@ typedef struct
 	GPIO_RegDef_t *p_GPIOx; // Holds base address of the GPIO port to which pin belongs
 	GPIO_PinConfig_t GPIO_PinConfig; // Holds GPIO pin config settings
 } GPIO_Handle_t;
+
+
+/**
+ * @GPIO_PIN_NUMBERS
+ * GPIO pin numbers
+ */
+#define GPIO_PIN_NO_0		0
+#define GPIO_PIN_NO_1		1
+#define GPIO_PIN_NO_2		2
+#define GPIO_PIN_NO_3		3
+#define GPIO_PIN_NO_4		4
+#define GPIO_PIN_NO_5		5
+#define GPIO_PIN_NO_6		6
+#define GPIO_PIN_NO_7		7
+#define GPIO_PIN_NO_8		8
+#define GPIO_PIN_NO_9		9
+#define GPIO_PIN_NO_10		10
+#define GPIO_PIN_NO_11		11
+#define GPIO_PIN_NO_12		12
+#define GPIO_PIN_NO_13		13
+#define GPIO_PIN_NO_14		14
+#define GPIO_PIN_NO_15		15
+
+
+/**
+ * @GPIO_PIN_MODES
+ * GPIO pin modes
+ */
+#define GPIO_MODE_IN 		0
+#define GPIO_MODE_OUT 		1
+#define GPIO_MODE_ALTFN 	2
+#define GPIO_MODE_ANALOG 	3
+#define GPIO_MODE_IN_FT  	4
+#define GPIO_MODE_IN_RT		5
+#define GPIO_MODE_IT_RFT	6
+
+
+/**
+ * @GPIO_PIN_PUPD
+ * GPIO pin pullup and pulldown
+ */
+#define GPIO_PIN_NO_PUPD	0
+#define GPIO_PIN_PU			1
+#define GPIO_PIN_PD			2
+
+
+/**
+ * @GPIO_OP_TYPES
+ * GPIO output types
+ */
+#define GPIO_OP_TYPE_PP		0
+#define GPIO_OP_TYPE_OD		1
+
+
+/**
+ * @GPIO_OP_SPEEDS
+ * GPIO output speeds
+ */
+#define GPIO_SPEED_LOW		0
+#define GPIO_SPEED_MEDIUM	1
+#define GPIO_SPEED_FAST		2
+#define GPIO_SPEED_HIGH		3
 
 
 // Driver's GPIO APIs
