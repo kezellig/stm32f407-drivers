e2265394163295d83ec0b6efc44f2d78f6337efb
Kaitlyn <kaitlyn@kaitlyn.works>
2024-05-21 09:48:16 +0800

    Add SPI CR2 and SR register bitfield macros

--- Drivers/Inc/stm32f407.h
+++ Drivers/Inc/stm32f407.h
@@ -418,6 +418,25 @@ typedef struct {
 
 
 /*** SPI register CR2 ***/
+#define SPI_CR2_RXDMAEN				0
+#define SPI_CR2_TXDMAEN				1
+#define SPI_CR2_SSOE				2
+#define SPI_CR2_FRF					4
+#define SPI_CR2_ERRIE				5
+#define SPI_CR2_RXNEIE				6
+#define SPI_CR2_TXEIE				7
+
+
+/*** SPI register SR ***/
+#define SPI_SR_RXE					0
+#define SPI_SR_TXE					1
+#define SPI_SR_CHSIDE				2
+#define SPI_SR_UDR					3
+#define SPI_SR_CRCERR				4
+#define SPI_SR_MODF					5
+#define SPI_SR_OVR					6
+#define SPI_SR_BSY					7
+#define SPI_SR_FRE					8
 
 
 
